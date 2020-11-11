8f16ad75164fa40caf48a842a2ad0e34ba420a49
daniela <danielabonillac@hotmail.com>
2020-11-11 01:16:02 +0100

    Updates

---
+++ precache-manifest.d5948b5ad5d9cf9d0f53b23077c021a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa67e78cfcd8248f504824b8880b247a",
+    "url": "/Geojson/index.html"
+  },
+  {
+    "revision": "fc25b30685abaaa781b2",
+    "url": "/Geojson/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "fbae0ffbf2aadc8b687d",
+    "url": "/Geojson/static/css/main.a9e139c9.chunk.css"
+  },
+  {
+    "revision": "fc25b30685abaaa781b2",
+    "url": "/Geojson/static/js/2.ecf2eba4.chunk.js"
+  },
+  {
+    "revision": "3161bd9cfcdda7f6e78af657f0b17c52",
+    "url": "/Geojson/static/js/2.ecf2eba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbae0ffbf2aadc8b687d",
+    "url": "/Geojson/static/js/main.8be4a87f.chunk.js"
+  },
+  {
+    "revision": "73ec6455df5cd5369e42",
+    "url": "/Geojson/static/js/runtime-main.54c5857f.js"
+  }
+]);
